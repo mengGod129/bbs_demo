8c96a49107ad43f2f4987fcdac41e9bc58570372
mrddy <me@mrddy.com>
2020-02-14 16:06:39 +0800

    init repo

---
+++ src/main/java/com/bil/spring_boot/bbsdemo/BbsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bil.spring_boot.bbsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bil/spring_boot/bbsdemo/BbsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bil.spring_boot.bbsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
